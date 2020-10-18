910cb6424c10397c2f98155a0a0fdf45a16b501d
temilaj <temilaj@hotmail.com>
2020-10-18 07:41:18 +0100

    1 neuron 3 inputs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
+inputs = [1, 2, 3, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2
 
 # sum of inputs * weight + bias
 output = inputs[0] * weights[0] + inputs[1] * \
-    weights[1] + inputs[2] * weights[2] + bias
+    weights[1] + inputs[2] * weights[2] + inputs[3] * weights[3] + bias
 print(output)
